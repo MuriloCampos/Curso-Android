ab2759fb4325c313e037ed2b1c3682206f2190e2
Murilo Campos <murilo13campos@gmail.com>
2016-03-31 13:42:06 -0300

    Calculadora simples desenvolvida em sala de aula

---
+++ CalculaTec/app/src/main/java/net/mcampos/calculatec/CalculadoraActivity.java
@@ -0,0 +1,91 @@
+package net.mcampos.calculatec;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CalculadoraActivity extends AppCompatActivity {
+
+    private Calculadora calc;
+
+    private boolean usuarioEstaDigitandoUmNumero;
+    private boolean separadorDecimalFoiDigitado;
+    private TextView txtVisor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+
+        calc = new Calculadora();
+        usuarioEstaDigitandoUmNumero = false;
+        separadorDecimalFoiDigitado = false;
+
+        txtVisor = (TextView)findViewById(R.id.txtVisor);
+        txtVisor.setText("0");
+    }
+
+    public void onClickNumeros(View v) {
+        Button botaoTocado = (Button)v;
+        String digito = botaoTocado.getText().toString();
+
+        String textoNoVisor = txtVisor.getText().toString();
+
+        if (!usuarioEstaDigitandoUmNumero || textoNoVisor.equals("0")) {
+            txtVisor.setText(digito);
+            if (!digito.equals("0")) {
+                usuarioEstaDigitandoUmNumero =  true;
+            }
+        } else {
+            txtVisor.setText(textoNoVisor + digito);
+        }
+    }
+
+    public void onClickOperacoes (View v) {
+        Button botaoTocado = (Button )v;
+        String operacao = botaoTocado.getText().toString();
+
+        if (operacao.equals(",") && !separadorDecimalFoiDigitado) {
+            separadorDecimalFoiDigitado = true;
+            if(!usuarioEstaDigitandoUmNumero)
+                txtVisor.setText("0" + ",");
+            else
+                txtVisor.setText(txtVisor.getText().toString() + ",");
+            usuarioEstaDigitandoUmNumero = true;
+        } else if (!operacao.equals(",")) {
+            String valorSemVirgula = txtVisor.getText().toString().replace(',','.');
+            calc.setOperando(Double.parseDouble(valorSemVirgula));
+            calc.realizarOperacao(operacao);
+
+            String textoResultado = String.valueOf(calc.getOperando());
+
+            if (textoResultado.endsWith(".0")) {
+                textoResultado = textoResultado.substring(0, textoResultado.length() - 2);
+            }
+
+            txtVisor.setText(textoResultado.replace('.',','));
+
+            usuarioEstaDigitandoUmNumero = false;
+            separadorDecimalFoiDigitado = false;
+        }
+    }
+
+    public void onClickMemoria(View v) {
+        Button botaoTocado = (Button)v;
+        String operacaoMemoria = botaoTocado.getText().toString();
+
+        String valorSemVirgula = txtVisor.getText().toString().replace(',','.');
+
+        calc.setOperando(Double.parseDouble(valorSemVirgula));
+        calc.realizarOperacaoDeMemoria(operacaoMemoria);
+
+        usuarioEstaDigitandoUmNumero = false;
+    }
+}
